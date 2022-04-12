88d0eee6fd77e084fd2f0b54bb34cf9708603061
Guus Noij <guus.noij@perplex.nl>
2022-04-12 15:25:12 +0200

    added tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    content: ['./src/**/*.{html, vue,js}'],
+    theme: {
+      colors: {
+        'blue': '#3311bb',
+        'lightblue': '#f0f6f8', 
+        'pink': '#ef4189',
+        'lightpink': '#fdebeb',
+        'dark': '#00061a',
+        'gray': '#e8eff2'
+      },
+      fontFamily: {
+        sans: ['Averta', 'sans-serif'],
+        serif: ['Averta', 'sans-serif'],
+      },
+      extend: {
+        spacing: {
+        },
+        borderRadius: {
+        }
+      }
+    },
+  }
