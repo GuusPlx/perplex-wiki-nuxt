5ca8f177cf21560a43ee32126cb2281a7904ffe6
Guus Noij <guus.noij@perplex.nl>
2022-04-12 15:11:03 +0200

    changed favicon and page Title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'perplex-blog',
+    title: 'Perplex research - blog',
     htmlAttrs: {
       lang: 'en',
     },
