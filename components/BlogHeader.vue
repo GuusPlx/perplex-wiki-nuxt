0a45060d17d3beec1ee7feff4d91047901188a5f
perplex\Mike <mike@perplex.nl>
2022-04-13 10:57:14 +0200

    added var

--- components/BlogHeader.vue
+++ components/BlogHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="bg-dark py-16 px-8 h-5/6">
+    <header class="bg-dark py-16 px-8 h-5/6 header-container">
         <section class="text-white container-blog mx-auto mt-40">
             <slot></slot>
         </section>
@@ -10,5 +10,9 @@
 <script setup></script>
 
 <style lang="postcss" scoped>
-    
+    .header-containter{
+        display: grid;
+        grid-template-columns: 1fr var(--blog-container-width) 1fr;
+        grid-template-row: auto;
+    }
 </style>
