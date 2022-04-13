395b0370a5c6b660c85d61a8c7c94deda1abe41e
Guus Noij <guus.noij@perplex.nl>
2022-04-13 09:14:14 +0200

    remove tutorial component

--- components/BlogHeader.vue
+++ components/BlogHeader.vue
@@ -1,14 +1,9 @@
-<!-- Please remove this file from your project -->
 <template>
-    <header class="bg-dark py-16 px-8 h-5/6">
-        
-        <section class="text-white max-w-3xl mx-auto mt-40">
-            <slot></slot>
-        </section>
-    </header>
+  <header class="h-5/6 bg-dark py-16 px-8">
+    <section class="mx-auto mt-40 max-w-3xl text-white">
+      <slot></slot>
+    </section>
+  </header>
 </template>
 
-<script setup>
-  
-
-</script>
+<script setup></script>
