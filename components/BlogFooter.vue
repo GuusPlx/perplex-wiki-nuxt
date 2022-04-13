0d43ccaae3bc51061bfed287c4b3851b1598e34c
Guus Noij <guus.noij@perplex.nl>
2022-04-13 10:11:34 +0200

    Added footer socials

--- components/BlogFooter.vue
+++ components/BlogFooter.vue
@@ -1,11 +1,23 @@
 <template>
-    <footer class="bg-gray py-8 mt-20">
-        <div class="container mx-auto grid grid-cols-2">
-            <div>
-                © Perplex Digital bv.
-            </div>
-        </div>        
-    </footer>
+  <footer class="mt-20 bg-gray py-8">
+    <div class="container mx-auto grid grid-cols-1 gap-5 md:grid-cols-2">
+      <div>© Perplex Digital bv.</div>
+      <div>
+        <ul class="flex justify-end gap-5">
+          <li><NuxtLink to="/">Latest posts</NuxtLink></li>
+          <li>
+            <a href="https://www.facebook.com/perplex.nl" target="_blank"
+              >Facebook</a
+            >
+          </li>
+          <li>
+            <a href="https://www.instagram.com/perplexarnhem/" target="_blank"
+              >Instagram</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </footer>
 </template>
-<script setup>
-</script>
+<script setup></script>
