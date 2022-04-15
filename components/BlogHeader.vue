6075733e9e401a499e092ae29e1e99692e4b079b
Guus Noij <guus.noij@perplex.nl>
2022-04-15 16:19:41 +0200

    blog header fix for short titles

--- components/BlogHeader.vue
+++ components/BlogHeader.vue
@@ -1,12 +1,11 @@
 <template>
   <header class="header-container h-5/6 bg-dark py-16">
-    <div class="container-blog mx-auto mt-40 text-white sm:col-span-1 sm:col-start-2">
+    <div class="container-blog mx-auto mt-40 w-full text-white sm:col-span-1 sm:col-start-2">
       <slot></slot>
     </div>
-    <div class="container-blog mx-auto w-full flex relative mt-8 -mb-16 sm:col-start-2 xl:col-start-3 empty:hidden">
+    <div class="container-blog relative mx-auto mt-8 -mb-16 flex w-full empty:hidden sm:col-start-2 xl:col-start-3">
       <slot name="author"></slot>
     </div>
-    
   </header>
 </template>
 
@@ -18,9 +17,9 @@
   grid-template-columns: 1fr;
   grid-template-row: 1fr auto;
 
-  @screen sm{
-      grid-template-columns: 1fr var(--blog-container-width) 1fr;
-      grid-template-row: auto;
+  @screen sm {
+    grid-template-columns: 1fr var(--blog-container-width) 1fr;
+    grid-template-row: auto;
   }
 }
 </style>
